6ec64bb1023049e85d958a2deb2fe2e7c0dc9535
shofiullah <pop.shofi@mail.com>
2019-07-02 16:08:19 +0600

    initial

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,14 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <audioplayers/AudioplayersPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AudioplayersPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioplayersPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
 }
 
 @end
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.deepPurple,
+  ];
+
+
+  _playSound(int index){
+    final player = AudioCache();
+    player.play('note${index + 1}.wav');
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: List.generate(7, (index) {
+              return Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    _playSound(index);
+                  },
+                  child: Material(
+                    color: colors[index],
+                  ),
+                ),
+              );
+            }),
+          ),
         ),
       ),
     );
